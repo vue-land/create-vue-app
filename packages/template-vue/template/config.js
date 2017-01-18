9d62be861f121a3bd0a82b0dd318fde453f4617e
EGOIST <0x142857@gmail.com>
2017-01-18 18:09:36 +0800

    split vendor code and app code

--- packages/template-vue/template/config.js
+++ packages/template-vue/template/config.js
@@ -5,5 +5,17 @@ module.exports = {
     plugins: [
       require('postcss-nested')()
     ]
+  },
+  webpack(config, options, webpack) {
+    if (options.production) {
+      // Split vendor code and app code
+      config.entry.vendor = ['vue']
+      config.plugins.push(
+        new webpack.optimize.CommonsChunkPlugin({
+          names: ['vendor', 'manifest']
+        })
+      )
+    }
+    return config
   }
 }
