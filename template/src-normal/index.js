e96a0b762075438f07f1aee94adc2d6f4c34ee36
EGOIST <0x142857@gmail.com>
2017-03-13 20:36:37 +0800

    trim empty lines

--- template/src-normal/index.js
+++ template/src-normal/index.js
@@ -1,12 +1,10 @@
 import Vue from 'vue'
 import App from 'components/App'
-
-<%_ if (pwa) { -%>
+<% if (pwa) { %>
 if (process.env.NODE_ENV === 'production') {
   require('./pwa')
 }
 <% } %>
-
 new Vue({
   el: '#app',
   render: h => h(App)
