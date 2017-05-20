9fc38107dad99218de2668938d84f2c5ceda5e26
EGOIST <0x142857@gmail.com>
2017-05-20 13:31:36 +0800

    update default component

--- packages/template-vue/template/src/components/App.vue
+++ packages/template-vue/template/src/components/App.vue
@@ -3,44 +3,96 @@
     <div class="banner">
       <img
         src="https://vuejs.org/images/logo.png"
-        width="300"
+        width="100"
         alt="vue"
+        class="logo"
       />
-      <h1>Hello, {{ time }}</h1>
+      <h1>Welcome to Vue.js</h1>
+    </div>
+    <div class="bottom">
+      <p>
+        To get started, edit <code>./src/index.js</code> and save to reload.<br/>
+        <span :class="$style.fade">
+          Checkout <code>./README.md</code> for more usages.
+        </span>
+      </p>
     </div>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'app',
-    data() {
-      return {time: new Date().toLocaleString()}
-    },
-    created() {
-      setInterval(() => {
-        this.time = new Date().toLocaleString()
-      }, 1000)
-    }
+    name: 'app'
   }
 </script>
+
+<style>
+  html, body {
+    height: 100%;
+  }
+
+  body {
+    margin: 0;
+    font: 14px/1.4 -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
+  }
+
+  code {
+    font-family: Menlo, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace, serif;
+    font-size: 0.9em;
+    white-space: pre-wrap;
+    color: #2c3e50;
+  }
+
+  code::before, code::after {
+    content: '`';
+  }
+</style>
 
 <style scoped>
   #app {
     display: flex;
-    align-items: center;
     justify-content: center;
+    flex-direction: column;
+    height: 100%;
+    text-align: center;
   }
 
   #app h1 {
-    color: red;
+    color: #2c3e50;
+    font-weight: 300;
   }
 
   .banner {
     height: 50%;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
   }
 
   .bottom {
     height: 50%;
+    background-color: #f6f6f6;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    font-size: 24px;
+    font-weight: 300;
+  }
+
+  .logo {
+    animation: spin 4s 1s infinite linear
+  }
+
+  @keyframes spin {
+    from {transform:rotate(0deg);}
+    to {transform:rotate(360deg);}
+  }
+</style>
+
+<style module>
+  .fade {
+    font-size: 14px;
   }
 </style>
