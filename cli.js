4b2ccb666ce6965174c68a225ca6a71052c87a0c
EGOIST <0x142857@gmail.com>
2017-05-22 23:30:52 +0800

    add standalone cli

--- cli.js
+++ cli.js
@@ -1,6 +1,33 @@
 #!/usr/bin/env node
 const path = require('path')
 const spawn = require('cross-spawn')
+const yargs = require('yargs')
+const pkg = require('./package')
+
+const cli = yargs
+  .option('sao-version', {
+    desc: 'Display the version of package "sao"'
+  })
+  .option('template-version', {
+    desc: 'Display the version of package "template-vue"'
+  })
+  .example('$0 my-project', 'Generate a new project in "my-project" folder')
+  .epilogue('If you have any problems, do not hesitate to file an issue:\n https://github.com/egoist/create-vue-app/issues/new')
+  .alias('v', 'version')
+  .alias('h', 'help')
+  .version(pkg.version)
+  .help()
+  .argv
+
+if (cli.templateVersion) {
+  console.log(`template-vue@${require('template-vue/package').version}`)
+  process.exit()
+}
+
+if (cli.saoVersion) {
+  console.log(`sao@${require('sao/package').version}`)
+  process.exit()
+}
 
 const sao = require.resolve('sao/bin/sao')
 
