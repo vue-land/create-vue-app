c766dc61c6c02f6f66495eba15622cb3ba493da3
EGOIST <0x142857@gmail.com>
2017-05-23 22:41:16 +0800

    fix indent

--- packages/template-vue/template/test/test.js
+++ packages/template-vue/template/test/test.js
@@ -3,9 +3,9 @@ import Vue from 'vue'
 import App from '../src/components/App.vue'
 
 test('renders', t => {
-	const vm = new Vue(App).$mount()
-	const tree = {
-		$el: vm.$el.outerHTML
-	}
-	t.snapshot(tree)
+  const vm = new Vue(App).$mount()
+  const tree = {
+    $el: vm.$el.outerHTML
+  }
+  t.snapshot(tree)
 })
