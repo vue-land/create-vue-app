3a84a93404f19c069af052f5bc5190c449d00f3e
EGOIST <0x142857@gmail.com>
2017-05-23 22:44:10 +0800

    add test cases for unit tests

--- packages/template-vue/test.js
+++ packages/template-vue/test.js
@@ -7,7 +7,7 @@ const template = {
 
 test('defaults', async t => {
   const res = await sao.mockPrompt(template, {})
-  t.deepEqual(res.fileList, [ 
+  t.deepEqual(res.fileList, [
     '.editorconfig',
     '.gitignore',
     'README.md',
@@ -21,6 +21,53 @@ test('defaults', async t => {
     'static/favicon.ico',
     'static/icons/android-chrome-192x192.png',
     'static/icons/android-chrome-512x512.png',
-    'static/manifest.json' 
+    'static/manifest.json'
+  ])
+})
+
+test('use ava', async t => {
+  const res = await sao.mockPrompt(template, {
+    test: 'ava'
+  })
+  t.deepEqual(res.fileList, [
+    '.editorconfig',
+    '.gitignore',
+    'README.md',
+    'index.ejs',
+    'package.json',
+    'poi.config.js',
+    'src/components/App.vue',
+    'src/index.js',
+    'src/polyfills.js',
+    'src/pwa.js',
+    'static/favicon.ico',
+    'static/icons/android-chrome-192x192.png',
+    'static/icons/android-chrome-512x512.png',
+    'static/manifest.json',
+    'test/helpers/setup.js',
+    'test/test.js'
+  ])
+})
+
+test('use ava', async t => {
+  const res = await sao.mockPrompt(template, {
+    test: 'karma'
+  })
+  t.deepEqual(res.fileList, [
+    '.editorconfig',
+    '.gitignore',
+    'README.md',
+    'index.ejs',
+    'package.json',
+    'poi.config.js',
+    'src/components/App.test.js',
+    'src/components/App.vue',
+    'src/index.js',
+    'src/polyfills.js',
+    'src/pwa.js',
+    'static/favicon.ico',
+    'static/icons/android-chrome-192x192.png',
+    'static/icons/android-chrome-512x512.png',
+    'static/manifest.json'
   ])
 })
