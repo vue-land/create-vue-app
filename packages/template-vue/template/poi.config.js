efdcf06bdef33c1097b565f5031e0cf36742885a
EGOIST <0x142857@gmail.com>
2017-07-03 13:38:51 +0800

    fix typo

--- packages/template-vue/template/poi.config.js
+++ packages/template-vue/template/poi.config.js
@@ -8,7 +8,7 @@ module.exports = {
   ],
   html: {
     title: pkg.productName,
-    description: pkg.descrption,
+    description: pkg.description,
     template: path.join(__dirname, 'index.ejs')
   },
   postcss: {
